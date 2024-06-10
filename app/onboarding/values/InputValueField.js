cab9615bd0303bd82339665c054957c6858dfe58
Javier <51163175+jfa94@users.noreply.github.com>
2024-06-10 14:56:34 +0100

    Added responsive design, fixed theme colour on mobile

--- app/onboarding/values/InputValueField.js
+++ app/onboarding/values/InputValueField.js
@@ -5,14 +5,14 @@ const alertIsOriginal = () => {
 }
 
 export function InputValueField({id, removeInput = alertIsOriginal}) {
-    return <div className="form-field flex flex-col md:flex-row items-start gap-2 md:gap-4 mb-4">
+    return <div className="form-field grid grid-cols-6 md:grid-cols-[1fr_1fr_1fr_1fr_50px] grid-rows-[auto_1fr] md:grid-rows-1 items-start gap-2 md:gap-4 mb-8">
 
         <input type="text"
                name={`value${id}Name`}
                id={`value${id}Name`}
                placeholder="New Value"
                autoComplete="off"
-               className="custom-label text-xl justify-start basis-1/5 overflow-ellipsis"
+               className="custom-label text-xl overflow-ellipsis col-span-5 md:col-span-1"
                required
         />
 
@@ -20,13 +20,13 @@ export function InputValueField({id, removeInput = alertIsOriginal}) {
                   id={`value${id}Description`}
                   placeholder="Description"
                   autoComplete="off"
-                  className="textarea textarea-lg textarea-bordered w-full"
+                  className="textarea textarea-lg textarea-bordered col-span-6 md:col-span-3"
                   rows="3"
                   required
         />
 
         <div onClick={() => removeInput(id)}
-             className="w-12 h-10 inline-flex items-center justify-center rounded-md bg-red-300/10 border-2 border-red-700/50 text-primary hover:cursor-pointer"
+             className="w-12 h-10 col-span-1 col-start-6 md:col-start-5 row-start-1 inline-flex items-center justify-center rounded-md bg-red-300/10 border-2 border-red-700/50 text-primary hover:cursor-pointer"
         >
             <svg xmlns="http://www.w3.org/2000/svg"
                  viewBox="0 0 24 24"
--- app/onboarding/values/ValuesForm.js
+++ app/onboarding/values/ValuesForm.js
@@ -19,7 +19,7 @@ const ValuesForm = () => {
     }
 
     let RemoveInputFromArray = (id) => {
-        if(window.confirm('Are you sure?')) {
+        if (window.confirm('Are you sure?')) {
             setInputArray((prevState) => {
                 return prevState.filter((inputElement) => inputElement.key !== `${id}`)
             })
@@ -29,20 +29,24 @@ const ValuesForm = () => {
     return (
         <>
             <div className="flex flex-row justify-between mb-2">
-                <h2 className="subheading">Values</h2>
-                <div onClick={addInputToArray} className="btn btn-neutral btn-sm">
-                    Add
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-5 h-5 fill-white">
-                        <path
-                            d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM11 11H7V13H11V17H13V13H17V11H13V7H11V11Z"></path>
-                    </svg>
-                </div>
+                <h2 className="subheading text-2xl pt-0">Values</h2>
             </div>
             <form action={submitValuesForm} className="flex flex-col">
                 {inputArray}
-                <button type="submit" className="btn btn-primary min-w-32 self-end">
-                    Save
-                </button>
+
+                <div className="flex flex-col md:flex-row gap-4 md:justify-end md:mr-16">
+                    <div onClick={addInputToArray} className="btn btn-neutral min-w-36">
+                        Add
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-5 h-5 fill-white">
+                            <path
+                                d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM11 11H7V13H11V17H13V13H17V11H13V7H11V11Z"></path>
+                        </svg>
+                    </div>
+                    <button type="submit" className="btn btn-primary min-w-36">
+                        Save
+                    </button>
+                </div>
+
             </form>
 
         </>
