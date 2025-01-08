9d7ef18b1323c1841781d6261c9f767e70de4970
Javier <51163175+jfa94@users.noreply.github.com>
2025-01-08 15:20:38 +0000

    Started structure for survey response API call

--- app/survey-response/page.js
+++ app/survey-response/page.js
@@ -1,14 +1,17 @@
 "use client"
 import {useSearchParams} from "next/navigation"
+import {useEffect} from "react";
 
 export default function Page() {
     const searchParams = useSearchParams()
-    let userObject = {}
+    const responseData = searchParams.get("req")
+    const responseValue = searchParams.get("val")
 
-    if (searchParams.has('uid')) {
-        userObject = {uid: searchParams.get('uid')}
-    }
+    useEffect(() => {
+        // TODO: Create Lambda function that can get the data and call it here
+    }, [])
+
     return <main className="w-full h-96 mb-auto flex justify-center items-center">
-        <p>Hello, {userObject?.uid || 'you'}</p>
+        <p>Thank you! Your response has been captured.</p>
     </main>
 }
