f9a20bd04b555a925b376f9e892fb546e18fa758
Javier <51163175+jfa94@users.noreply.github.com>
2025-02-05 18:12:08 -0600

    Added Plausible event tag

--- components/ButtonWaitlist.js
+++ components/ButtonWaitlist.js
@@ -7,7 +7,7 @@ export default function ButtonWaitlist(props) {
     const {pending} = useFormStatus()
     const searchParams = useSearchParams()
 
-    return <button className="btn btn-primary btn-block mt-6"
+    return <button className="btn btn-primary btn-block mt-6 plausible-event-name=WaitingList"
                    type="submit"
                    disabled={pending}
     >
