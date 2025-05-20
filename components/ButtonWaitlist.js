246671bb483ec42152a4d35003a5c79b02175c3d
Javier <51163175+jfa94@users.noreply.github.com>
2025-05-20 10:21:51 +0100

    Added Google conversion tracking

--- components/ButtonWaitlist.js
+++ components/ButtonWaitlist.js
@@ -2,16 +2,27 @@
 
 import {useFormStatus} from "react-dom";
 import {useSearchParams} from "next/navigation"
+import { sendGTMEvent } from '@next/third-parties/google'
 import posthog from "posthog-js"
 
 export default function ButtonWaitlist(props) {
     const {pending} = useFormStatus()
     const searchParams = useSearchParams()
 
+    const handleClick = () => {
+        sendGTMEvent({
+            event: 'conversion',
+            value: {send_to: 'AW-17081447837/WxmzCK3Py8oaEJ3riNE_'}
+        })
+
+        posthog.capture("waitlist_signup_click")
+    }
+
+
     return <button className="btn btn-primary btn-block mt-6 plausible-event-name=WaitingList"
                    type="submit"
                    disabled={pending}
-                   onClick={() => posthog.capture("waitlist_signup_click")}
+                   onClick={handleClick}
     >
         {searchParams.get('success') ? 'Success!' : props.children}
     </button>
