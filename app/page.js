257b7a9b6a5304123a3049b9d98fcade59c04034
Javier <51163175+jfa94@users.noreply.github.com>
2025-05-23 16:41:32 +0100

    Updated Google conversion tracking again

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import {Suspense} from "react";
-import {GoogleTagManager} from "@next/third-parties/google"
+import {GoogleAnalytics} from "@next/third-parties/google"
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import FAQ from "@/components/FAQ";
@@ -13,7 +13,7 @@ import Statistics from "@/components/Statistics";
 export default function Page() {
     return (
         <>
-            <GoogleTagManager gtmId="AW-17081447837"/>
+            <GoogleAnalytics gaId="AW-17081447837"/>
             <Suspense fallback={<header className="bg-amber-50"></header>}>
                 <Header bgClass="bg-amber-50"></Header>
             </Suspense>
